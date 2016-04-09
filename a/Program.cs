34c3369b4f1eb7b546c2be1cc12e358d11f42a12
truong225 <truongmero225@gmail.com>
2016-04-09 23:22:23 +0700

    Program.cs

--- a/Program.cs
+++ a/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace a     //Tested
+namespace a     //Tested////////////////////////////////////////////////////////////////////////
 {
     static class Program
     {
